a12452e3b7386621bd97abc7a4d8771e2c2234dc
Rahul Ohol <nevergiupyou123@gmail.com>
2023-03-12 18:31:20 +0530

    middlewares

---
+++ middlewares/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const reqFilter= require('./middleware');
+const app = express();
+const route= express.Router();
+
+
+// app.use(reqFilter);
+route.use(reqFilter)
+
+app.get('/', (res, resp) => {
+    resp.send('Welcome to Home page')
+});
+
+app.get('/users', (res, resp) => {
+    resp.send('Welcome to Users page')
+});
+
+route.get('/about', (res, resp) => {
+    resp.send('Welcome to About page')
+});
+route.get('/contact', (res, resp) => {
+    resp.send('Welcome to contact page')
+});
+
+app.use('/',route);
+
+app.listen(5000)
